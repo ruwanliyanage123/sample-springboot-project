1f8568c54b9124e2904edce857e8a274b9340933
ruwanliyanage123 <ruwanliyanagegalle1994@gmail.com>
2022-05-20 22:34:14 +0530

    Init commit

---
+++ src/main/java/com/sample/sampleproject/SampleProjectApplication.java
@@ -0,0 +1,14 @@
+package com.sample.sampleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleProjectApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		SpringApplication.run(SampleProjectApplication.class, args);
+	}
+
+}

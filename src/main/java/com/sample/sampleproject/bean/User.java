9e6d4c3c6707e6313b068b26053bcaef0f696f61
ruwanliyanage123 <ruwanliyanagegalle1994@gmail.com>
2022-05-20 23:24:53 +0530

    Add bean for User Persistance

---
+++ src/main/java/com/sample/sampleproject/bean/User.java
@@ -0,0 +1,22 @@
+package com.sample.sampleproject.bean;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+
+    // standard constructors / setters / getters / toString
+}

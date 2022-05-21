73114dc45b984920f37d4c5c7243a6e7f1f8b8aa
ruwanliyanage123 <ruwanliyanagegalle1994@gmail.com>
2022-05-21 09:38:02 +0530

    Modified controller

--- src/main/java/com/sample/sampleproject/controller/EmployeeController.java
+++ src/main/java/com/sample/sampleproject/controller/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.sample.sampleproject.controller;
 
 import com.sample.sampleproject.model.Employee;
+import com.sample.sampleproject.service.api.EmployeeServiceAPI;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -8,23 +10,21 @@ import java.util.List;
 @RestController
 public class EmployeeController {
 
+    @Autowired
+    EmployeeServiceAPI employeeService;
+
     @PostMapping("/")
-    public void addEmployee(){
-        //add post method
+    public void addEmployee(Employee employee){
+        employeeService.addEmployee(employee);
     }
 
     @GetMapping("/employees")
     public List<Employee> getAllEmployees(){
-        return null;
-    }
-
-    @GetMapping("/employees/{id}")
-    public List<Employee> getAllEmployees(@PathVariable long empId){
-        return null;
+        return employeeService.getAllEmployee();
     }
 
     @DeleteMapping("/")
-    public void deleteEmployee(){
-
+    public void deleteEmployee(Long empId){
+        employeeService.deleteEmployee(empId);
     }
 }
